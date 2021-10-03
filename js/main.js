4b919eebc1d945ec128ee4823d6571407d519f8e
Troy Dorman <troydorman@gmail.com>
2021-10-02 19:57:01 -0600

    run type function on dom loaded

--- js/main.js
+++ js/main.js
@@ -8,16 +8,21 @@ const TypeWriter = function (txtElement, words, wait = 3000) {
   this.isDeleting = false;
 };
 
-
 // Type Method
+TypeWriter.prototype.type = function () {
+  console.log("hello");
+  setTimeout(() => {
+    this.type();
+  }, 500);
+};
 
 // Init on DOM load
 document.addEventListener("DOMContentLoaded", init);
 
 // init app
 function init() {
-    const txtElement = document.querySelector(".txt-type");
-    const words = JSON.parse(txtElement.getAttribute("data-words"));
-    const wait = txtElement.getAttribute("data-wait");
-    new TypeWriter(txtElement, words, wait);
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  new TypeWriter(txtElement, words, wait);
 }
