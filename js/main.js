b740fa4b93dddbb1bc72208f0386b2e4fa1cedba
Troy Dorman <troydorman@gmail.com>
2021-10-02 19:35:15 -0600

    created init function to create a new TypeWriter object instance

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ const TypeWriter = function (txtElement, words, wait = 3000) {
   this.type();
   this.isDeleting = false;
 };
+
+
+// Type Method
+
+// Init on DOM load
+document.addEventListener("DOMContentLoaded", init);
+
+// init app
+function init() {
+    const txtElement = document.querySelector(".txt-type");
+    const words = JSON.parse(txtElement.getAttribute("data-words"));
+    const wait = txtElement.getAttribute("data-wait");
+    new TypeWriter(txtElement, words, wait);
+}
